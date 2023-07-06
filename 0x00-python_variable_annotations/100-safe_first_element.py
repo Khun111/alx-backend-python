51acc7fa71e47dd4676d8f66a68c0d35c405a6f7
Oluwatobi <amure387@gmail.com>
2023-07-06 20:37:39 +0000

    feat vompleted advance tasks

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Module for typed annotations'''
+from typing import Sequence, Any, Union
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    '''Function to annotate'''
+    if lst:
+        return lst[0]
+    else:
+        return None
