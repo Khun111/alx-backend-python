ccfd70d4df66f40cb9d8d4a42240623bea7b7512
Oluwatobi <amure387@gmail.com>
2023-07-06 21:16:13 +0000

    fix: name T is not defined

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''Module for typed annotations'''
-from typing import Mapping, Any, Union
-
+from typing import TypeVar, Mapping, Any, Union
+T = TypeVar('T')
 
 def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None
 ) -> Union[Any, T]:
