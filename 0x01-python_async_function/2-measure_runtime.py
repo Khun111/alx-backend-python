043186de388e9f24f93733f7639da6eb0f8671da
Oluwatobi <amure387@gmail.com>
2023-07-10 12:11:43 +0000

    fix: documentation

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-'''Module for wait_n'''
+'''Module for measure_time'''
 import asyncio
 import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
+    '''Function for measure_time'''
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
     end_time = time.time()
--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -5,6 +5,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
+    '''Module for task_wait_random'''
     loop = asyncio.get_event_loop()
     task = loop.create_task(wait_random(max_delay))
     return task
